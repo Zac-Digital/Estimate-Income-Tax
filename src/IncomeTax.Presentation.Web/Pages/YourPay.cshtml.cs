1b0ef5bfd5eb840874f50b32b454e3ebd03bb6c9
Zac-Digital <119302767+Zac-Digital@users.noreply.github.com>
2025-09-20 23:17:36 +0100

    feat: salary page with error state validation

---
+++ src/IncomeTax.Presentation.Web/Pages/YourPay.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IncomeTax.Presentation.Web.Pages;
+
+public class YourPay : PageModel
+{
+    public readonly string[] Options = ["Yearly", "Monthly", "Every 4 weeks", "Weekly", "Daily", "Hourly"];
+
+    [BindProperty] 
+    [Required]
+    public double? Cost { get; set; }
+    
+    [BindProperty] 
+    public string Frequency { get; set; } = null!;
+    
+    public bool ErrorCost { get; private set; }
+    public bool ErrorFrequency { get; private set; }
+
+    public IActionResult OnPost()
+    {
+        if (!ModelState.IsValid)
+        {
+            ErrorCost = ModelState[nameof(Cost)]?.Errors.Count > 0;
+            ErrorFrequency = ModelState[nameof(Frequency)]?.Errors.Count > 0;
+            
+            return Page();
+        }
+
+        return Page(); // TODO: Next Page
+    }
+}
