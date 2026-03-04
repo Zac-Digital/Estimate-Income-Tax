332c35fc86ba2f688e6ada5cc2139afa627500be
Zac-Digital <119302767+Zac-Digital@users.noreply.github.com>
2026-03-04 22:01:00 +0000

    ci: run with 1 worker

--- test/IncomeTax.Presentation.Web.Functional/playwright.config.ts
+++ test/IncomeTax.Presentation.Web.Functional/playwright.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
     testDir: './tests',
     fullyParallel: true,
     forbidOnly: !!process.env.CI,
-    retries: process.env.CI ? 1 : 0,
+    retries: 1,
     reporter: [['html', {open: 'never'}], ['list']],
 
     use: {
